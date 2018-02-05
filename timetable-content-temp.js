5dd4437098e81a03a8efee663699e1d51e521ac3
Jahanvi Hooda <jahanvi_hooda@hotmail.com>
2018-02-05 18:01:06 +0000

    adjusted link

--- timetable-content-temp.js
+++ timetable-content-temp.js
@@ -1,3 +1,3 @@
 (function ($) {
-    $('head').append('<link rel="stylesheet" type="text/css" href="http://fes.yorku.ca/files/src/timetable_print.css"> media="print"');
+    $('head').append('<link rel="stylesheet" type="text/css" href="/files/src/timetable_print.css"> media="print"');
 })(jQuery);
